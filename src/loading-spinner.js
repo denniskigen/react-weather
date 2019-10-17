ef26caf19f63b37ac01c4c8c584baff5b6853f57
Dennis Kigen <kigen.work@gmail.com>
2019-10-18 00:04:45 +0300

    Add loading spinner

---
+++ src/loading-spinner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const useStyles = makeStyles(theme => ({
+  progress: {
+    margin: theme.spacing(2),
+  },
+}));
+
+export default function CircularIndeterminate() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <CircularProgress className={classes.progress} />
+    </div>
+  );
+}
