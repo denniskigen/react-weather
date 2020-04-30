78741b475ae2fb7411ad2501cb425cd202bf11a5
Dennis Kigen <kigen.work@gmail.com>
2020-04-30 12:22:17 +0300

    Remove orphaned import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
