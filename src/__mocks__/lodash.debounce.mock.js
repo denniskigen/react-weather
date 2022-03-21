3929cb1ab0a39e597e13076a218628f064dac6a6
Dennis Kigen <kigen.work@gmail.com>
2022-03-22 01:03:35 +0300

    (test) Remove unused debounce mock + minor fixes

--- src/__mocks__/lodash.debounce.mock.js
+++
@@ -1,9 +0,0 @@
-export default (fn, ms) => {
-  let timeoutId = 0;
-
-  return (...args) => {
-    clearTimeout(timeoutId);
-
-    timeoutId = setTimeout(() => fn(...args), ms);
-  };
-};
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -7,29 +7,23 @@ import {
 } from '../test/data/weather-data';
 
 export const handlers = [
-  rest.get(
-    'https://api.openweathermap.org/data/2.5/weather',
-    (req, res, ctx) => {
-      const searchQuery = req.url.searchParams.get('q');
+  rest.get(/\/weather\//, (req, res, ctx) => {
+    const searchQuery = req.url.searchParams.get('q');
 
-      if (searchQuery === 'Rio de Janeiro') {
-        return res(ctx.json(mockSearchWeatherData));
-      }
+    if (/Rio de Janeiro/i.exec(searchQuery)) {
+      return res(ctx.json(mockSearchWeatherData));
+    }
 
-      return res(ctx.json(mockWeatherData));
-    },
-  ),
+    return res(ctx.json(mockWeatherData));
+  }),
 
-  rest.get(
-    'https://api.openweathermap.org/data/2.5/forecast',
-    (req, res, ctx) => {
-      const searchQuery = req.url.searchParams.get('q');
+  rest.get(/\/forecast\//, (req, res, ctx) => {
+    const searchQuery = req.url.searchParams.get('q');
 
-      if (searchQuery === 'Rio de Janeiro') {
-        return res(ctx.json(mockSearchForecastData));
-      }
+    if (/Rio de Janeiro/i.exec(searchQuery)) {
+      return res(ctx.json(mockSearchForecastData));
+    }
 
-      return res(ctx.json(mockForecastData));
-    },
-  ),
+    return res(ctx.json(mockForecastData));
+  }),
 ];
