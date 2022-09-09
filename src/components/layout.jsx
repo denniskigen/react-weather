87c6eb0271bf3c171c1cf6057bf2dbf4da39c046
Dennis Kigen <kigen.work@gmail.com>
2022-09-09 23:16:41 +0300

    (fix) Fix issue where UI would cut off beyond viewport size

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -6,7 +6,7 @@ import NavBar from './navbar';
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-full">
       <NavBar />
       <div className="flex-grow">{children}</div>
       <Footer />
