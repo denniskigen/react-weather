937b082e4c6f8ca0781919a8dda6c2b681f50785
Dennis Kigen <kigen.work@gmail.com>
2022-09-09 18:35:40 +0300

    (chore) Add GitHub Actions Jest reporter

--- jest.config.js
+++ jest.config.js
@@ -102,7 +102,7 @@ module.exports = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: ['default', 'github-actions'],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
